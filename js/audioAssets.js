7c8034b487f953461a52fda0d17465dc04294363
System Administrator <root@amazon.com>
2016-12-31 00:02:46 -0800

    First check-in with basic control of turning on/off heelight.

---
+++ js/audioAssets.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// Audio Source
+var audioData = [
+    {
+        'title' : 'Turn on',
+        'url' : 'https://s3.amazonaws.com/aws-website-lielu-4nbti/40.wav'
+    },
+    {
+        'title' : 'Turn off',
+        'url' : 'https://s3.amazonaws.com/aws-website-lielu-4nbti/40.wav'
+    }
+];
+
+module.exports = audioData;
